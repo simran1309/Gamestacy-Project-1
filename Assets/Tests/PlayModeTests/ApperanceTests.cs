50a3658b9f6708dea82723bfeb1520a53f79b426
Simran Banga <82326153+simran1309@users.noreply.github.com>
2022-09-29 17:24:59 +0530

    Add files via upload

---
+++ Assets/Tests/PlayModeTests/ApperanceTests.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class ApperanceTests
+{
+ /*   private SphereClass sphereClass1;
+    private AppearanceManager ap;
+
+    private ScriptableCube cube;
+    private MeshRenderer theRenderer;
+    [SerializeField] private Rotation rotation;
+    private float RotateSpeed;
+
+
+    [SetUp]
+    public void SetUp()
+    {
+        ap = new GameObject().AddComponent<AppearanceManager>();
+        rotation = new GameObject().AddComponent<Rotation>();
+
+        cube = ScriptableObject.CreateInstance<ScriptableCube>();
+        sphereClass1 = new SphereClass
+        {
+            color = Color.white,
+
+        };
+        cube.sphereData = new SphereClass[]
+        {
+          sphereClass1
+        };
+
+        theRenderer = ap.gameObject.AddComponent<MeshRenderer>();
+        theRenderer.sharedMaterial = new Material(Shader.Find("Standard"));
+      
+
+    }
+
+    [UnityTest]
+    public IEnumerator TestColor()
+    {
+       
+
+            ap.ColorChange(0, cube, theRenderer);
+
+            Assert.AreEqual(theRenderer.sharedMaterial.color, cube.sphereData[0].color);
+
+        
+        yield return null;
+    }*/
+}
